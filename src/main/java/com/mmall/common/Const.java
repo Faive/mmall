ce8c585337ed333a23231ddb01b6bfadc640bfff
YangHong <JackYang1996@outlook.com>
2018-07-16 16:24:40 +0800

    testCommit
    testCommit

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -16,7 +16,8 @@ public class Const {
 
     public interface Role{
         int ROLE_CUSTOMER = 0;//普通用户
-        int ROLE_ADMIN = 1;//管理员用户    }
+        int ROLE_ADMIN = 1;//管理员用户
+    }
 
     public interface Cart{
         int CHECKED = 1;//选中
